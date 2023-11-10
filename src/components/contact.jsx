e6e039d4d9e8b938d9eaccbf0e8a3225d4aefa40
aniltimalsina <timalsinaanil96@gmail.com>
2023-11-09 22:07:08 -0500

    add contact page form

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,5 +1,32 @@
+import "../styles/contact.css";
 function contact() {
-  return <div>contact</div>;
+  return (
+    <>
+      <div>
+        <h1 className="form-title">Contact Us</h1>
+        <form>
+          <div className="names">
+            <label htmlFor="firstName">FirstName: </label>
+            <input type="text" id="firstName" name="firstName" required />
+            <label htmlFor="lastName">LastName: </label>
+            <input type="text" id="lastName" name="lastName" required />
+          </div>
+          <div className="remaining-inputs">
+            <div className="email-div">
+              <label htmlFor="email">Email: </label>
+              <input type="email" id="email" name="email" required />
+            </div>
+            <div>
+              <label htmlFor="message">Message: </label>
+              <textarea placeholder="Enter your message here ..." />
+            </div>
+
+            <button className="submit-btn">Submit</button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
 }
 
 export default contact;
